f87cccdf5a96c3e0d29ef7f25a03333b845e96cd
Yan Carlos <yan312005@gmail.com>
2025-06-30 15:07:51 -0300

    feat: using memo

--- src/app/components/productItem.tsx
+++ src/app/components/productItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,10 +8,26 @@ interface ProductItemProps {
   };
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.name} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) =>
+  Object.is(prevProps.product, nextProps.product)
+);
+
+/** O que o memo faz:
+ * Sem o memo: irá renderizar o componente se o componente pai mudar.
+ * com o memo: Só irá renderizar o componente se as props passadas para ele mudarem.
+ */
+
+/** Situações em que o memo pode ser utilizado:
+ * 1. componentes apenas para abstrair a interface de um componente complexo (Pure functional component).
+ * 2. componentes que renderizam muitas vezes (Ex: listas de itens).
+ * 3. componentes que re-renderizam com as mesmas props.
+ * 4. quando o componente tem o tamanho médio a grande, o memo vai trazer melhores resultados.
+ */
